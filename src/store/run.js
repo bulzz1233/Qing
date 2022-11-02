003ffbf52b6752c5faad3dddd76503ebe104fd86
吴 <bulzz1233@163.com>
2022-11-02 19:32:10 +0800

    修复bug

--- src/store/run.js
+++ src/store/run.js
@@ -71,9 +71,11 @@ export default {
             });
         },
         allData(state, obj) {
+            let arr=[]
             obj.forEach(element => {
-                state.All.push(element);
+                arr.push(element);
             });
+            state.All=arr
         },
     },
     state: {
